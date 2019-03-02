81cec8eae2ed56fbc12de7ec22235bfc54a42121
Tatsiana Gelahova <tgelahova99@gmail.com>
2019-03-02 22:55:19 +0300

    :hatching_chick: initial commit

---
+++ lab_4/server/src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+const port = 8080;
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get( "/", ( req, res ) => {
+    res.send( "Hello world!" );
+} );
+
+app.listen(port, () => {
+    // tslint:disable-next-line:no-console
+    console.log( `server started at http://localhost:${ port }` );
+} );
