79c321db058c35f0e964843a445358b31a0a8a51
Tatsiana Gelahova <tgelahova99@gmail.com>
2019-03-23 16:20:53 +0300

    initial setup for c# console app

---
+++ lab_5 - Консольное приложение C#/ConsoleApplication/ConsoleApplication/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication {
+    class Program {
+        static void Main(string[] args) {
+            pritnMenu();
+            Console.ReadKey();
+        }
+
+        static void pritnMenu() {
+            Console.WriteLine("---МЕНЮ ВЫБОРА----");
+            Console.WriteLine("" +
+                "1 - Добавить новое мороженое\n" +
+                "2 - Удалить мороженое\n" +
+                "3 - Отсортировать мороженое по цене\n" +
+                "4 - Найти мороженое\n" +
+            "");
+        }
+    }
+}
