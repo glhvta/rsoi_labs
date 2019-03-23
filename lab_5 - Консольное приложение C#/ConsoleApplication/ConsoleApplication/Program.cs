57c11201b8832f607b1dcd70264e3896c6143520
Tatsiana Gelahova <tgelahova99@gmail.com>
2019-03-23 19:49:03 +0300

    lab5: implement changeCost method

--- lab_5 - Консольное приложение C#/ConsoleApplication/ConsoleApplication/Program.cs
+++ lab_5 - Консольное приложение C#/ConsoleApplication/ConsoleApplication/Program.cs
@@ -21,7 +21,8 @@ namespace ConsoleApplication {
                     case 2: deleteIceCream(); break;
                     case 3: sortIceCream(); break;
                     case 4: findIceCream(); break;
-                    case 5: printAllIceCream(); break;
+                    case 5: changeIceCreamCost(); break;
+                    case 6: printAllIceCream(); break;
                     default: return;
                 }
             }
@@ -33,8 +34,9 @@ namespace ConsoleApplication {
                 "1 - Добавить новое мороженое\n" +
                 "2 - Удалить мороженое\n" +
                 "3 - Отсортировать мороженое по цене\n" +
-                "4 - Найти мороженое\n" + 
-                "5 - Вывести на экран все записи\n"
+                "4 - Найти мороженое\n" +
+                "5 - Изменить стоимость мороженого\n" + 
+                "6 - Вывести на экран все записи\n"
             );
         }
         static void addIceCream() {
@@ -48,7 +50,7 @@ namespace ConsoleApplication {
                 double cost = Convert.ToDouble(Console.ReadLine());
 
                 sw = new StreamWriter(pathToFile, true, System.Text.Encoding.Default);
-                sw.WriteLine(iceCreamsNumber++ + ": " + name + "  " + cost);
+                sw.WriteLine(iceCreamsNumber++ + ": " + name + "  $" + cost);
                 Console.WriteLine("Данные были добавлены успешно!");
             }
             catch {
@@ -97,10 +99,11 @@ namespace ConsoleApplication {
             return data;
         }
 
-        static string[] getIceCreamList() {
+        static IEnumerable<string> getIceCreamList()
+        {
             string iceCreamString = getIceCreamFromFile();
 
-            return iceCreamString.Split(new[] { '\r', '\n' });
+            return iceCreamString.Split(new[] { '\r', '\n' }).Where(line => line != ""); ;
         }
 
         static void printAllIceCream() {
@@ -108,11 +111,10 @@ namespace ConsoleApplication {
         }
 
         static void deleteIceCream() {
-            Console.WriteLine("Введите номер мороженого для поиска:  ");
+            Console.WriteLine("Введите номер мороженого для удаления:  ");
             char number = Convert.ToChar(Console.ReadLine());
 
             IEnumerable<string> newIceCreamList = getIceCreamList()
-                .Where(line => line != "")
                 .Where(line => line[0] != number);
 
             rewriteIceCreamData(String.Join("\n", newIceCreamList));
@@ -122,7 +124,7 @@ namespace ConsoleApplication {
             Console.WriteLine("Введите мороженое для поиска:  ");
             string iceCream = Console.ReadLine();
 
-            string[] iceCreamList = getIceCreamList();
+            IEnumerable<string> iceCreamList = getIceCreamList();
 
             foreach (string line in iceCreamList)
             {
@@ -134,8 +136,55 @@ namespace ConsoleApplication {
             }
         }
 
-        static void sortIceCream() {
-            Console.WriteLine("Sort Ice cream");
+        static int getCost (string data) {
+            int i = data.IndexOf('$');
+            string cost = data.Substring(i + 1);
+            return Convert.ToInt32(cost);
         }
+
+        static void sortIceCream() {
+            string[] iceCreams = getIceCreamList().ToArray<string>();
+
+            string temp;
+            for (int i = 0; i < iceCreams.Length - 1; i++)
+            {
+                for (int j = i + 1; j < iceCreams.Length; j++)
+                {    
+                    int cost1 = getCost(iceCreams[i]);
+                    int cost2 = getCost(iceCreams[j]);
+
+                    if (cost1 > cost2)
+                    {
+                        temp = iceCreams[i];
+                        iceCreams[i] = iceCreams[j];
+                        iceCreams[j] = temp;
+                    }
+                }
+            }
+
+            rewriteIceCreamData(String.Join("\n", iceCreams));
+        }
+
+        static void changeIceCreamCost() {
+            Console.WriteLine("Введите номер мороженого для изменения:  ");
+            char number = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Введите новую стоимость:  ");
+            string newCost = Console.ReadLine();
+
+            string[] iceCreams = getIceCreamList().ToArray<string>();
+
+            for (int i = 0; i < iceCreams.Length; i++ )
+            {
+                if (iceCreams[i][0] == number)
+                {
+                    string prevCost = Convert.ToString(getCost(iceCreams[i]));
+                    iceCreams[i] = iceCreams[i].Replace(prevCost, newCost);
+                }
+            }
+
+            rewriteIceCreamData(String.Join("\n", iceCreams));
+        }
+
     }
 }
