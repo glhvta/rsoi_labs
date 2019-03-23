ad0386ccd4be3344de6d497a2df83610cdf463eb
Tatsiana Gelahova <tgelahova99@gmail.com>
2019-03-23 16:38:18 +0300

    lab5: create templates for methods

--- lab_5 - Консольное приложение C#/ConsoleApplication/ConsoleApplication/Program.cs
+++ lab_5 - Консольное приложение C#/ConsoleApplication/ConsoleApplication/Program.cs
@@ -7,11 +7,22 @@ using System.Threading.Tasks;
 namespace ConsoleApplication {
     class Program {
         static void Main(string[] args) {
-            pritnMenu();
-            Console.ReadKey();
+            while (true) {
+                printMenu();
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1: printAllIceCream(); break;
+                    case 2: deleteIceCream(choice); break;
+                    case 3: sortIceCream(); break;
+                    case 4: findIceCream(choice); break;
+                    default: return;
+                }
+            }
         }
 
-        static void pritnMenu() {
+        static void printMenu() {
             Console.WriteLine("---МЕНЮ ВЫБОРА----");
             Console.WriteLine("" +
                 "1 - Добавить новое мороженое\n" +
@@ -19,6 +30,22 @@ namespace ConsoleApplication {
                 "3 - Отсортировать мороженое по цене\n" +
                 "4 - Найти мороженое\n" +
             "");
+        }
+
+        static void printAllIceCream() {
+            Console.WriteLine("Print all ice cream");
+        }
+
+        static void deleteIceCream(int number) {
+            Console.WriteLine("Delete ice cream");
+        }
+
+        static void findIceCream(int number) {
+            Console.WriteLine("Find ice cream");
+        }
+
+        static void sortIceCream() {
+            Console.WriteLine("Sort Ice cream");
         }
     }
 }
